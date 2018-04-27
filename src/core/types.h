920b4fa75ce3a0a70b860628adba77ea1183a448
Flk427 <flk427@gmail.com>
2018-04-27 12:02:16 +0400

    [+] Типы для поиска общих слов.

--- src/core/types.h
+++ src/core/types.h
@@ -1,10 +1,24 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <QMap>
+#include <QString>
+#include <QStringList>
+
 namespace CWTypes
 {
 
 enum DocumentType {Event, Journal};
+
+typedef struct
+{
+	int eventsOccuring;
+	int journalsOccuring;
+	QStringList events;
+	QStringList journals;
+} WordOccuring;
+
+typedef QMap<QString, WordOccuring> WordsOccuring;
 
 }
 
